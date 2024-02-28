3e05e802ea0b36fef566e27a6d7b3ff31ecdbb9b
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2024-02-28 05:12:42 +0300

    .

---
+++ TravelTripProje/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TravelTripProje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

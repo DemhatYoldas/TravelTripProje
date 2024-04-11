49410f833ca271b774a36b42e105ec82cdfb26cd
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2024-04-11 03:14:12 +0300

    .

--- TravelTripProje/Controllers/BlogController.cs
+++ TravelTripProje/Controllers/BlogController.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
 
 namespace TravelTripProje.Controllers
 {
     public class BlogController : Controller
     {
-        // GET: Blog
+        Context c = new Context();
         public ActionResult Index()
+        {
+            var bloglar=c.Blogs.ToList();
+            return View(bloglar);
+        }
+
+        public ActionResult BlogDetay()
         {
             return View();
         }
     }
-}
+} 

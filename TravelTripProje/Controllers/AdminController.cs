6811df1b764537321d790256cf2288000263161f
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2024-05-17 06:49:25 +0300

    .

---
+++ TravelTripProje/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class AdminController : Controller
+    {
+         Context c=new Context();
+        public ActionResult Index()
+        {
+            var deger=c.Blogs.ToList();
+            return View(deger);
+        }
+
+    }
+}

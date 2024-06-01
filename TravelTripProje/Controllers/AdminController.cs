2f89c9bf0082addd06802fdca3581ef1d28dda4f
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2024-06-01 12:58:36 +0300

    Tatil Seyahat Sitesi Blog Güncelleme

--- TravelTripProje/Controllers/AdminController.cs
+++ TravelTripProje/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -47,5 +48,17 @@ namespace TravelTripProje.Controllers
             }
         }
 
+        public ActionResult BlogGetir(int id) 
+        {
+            var veri = c.Blogs.Find(id);
+            return View("BlogGetir",veri); 
+        }
+        public ActionResult BlogGuncelle(Blog b)
+        {
+            var update=c.Blogs.Find(b);
+            c.Blogs.AddOrUpdate(update);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }

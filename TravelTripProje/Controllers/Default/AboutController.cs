7072df92c6e56f6350907793d9394d7d09d1183a
DEMHAT <DEMHAT@DESKTOP-JQG1QG1>
2025-06-27 04:37:10 +0300

    - Entity Framework Code First ile veritabanı bağlantısı yapıldı - Domain sınıfları oluşturuldu - UI layout düzenlendi - PartialView yapısı kuruldu

---
+++ TravelTripProje/Controllers/Default/AboutController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TravelTripProje.Controllers.Default
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialAbout()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialMission()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ TravelTripProje/Controllers/Default/DefaultController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TravelTripProje.Controllers.Default
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFacilities()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialStory()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialJourney()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialTeam()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialTestimonial()
+        {
+            return PartialView();   
+        }
+
+        public PartialViewResult PartialBlog()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+    }
+}
